a887c6b827d85d94443bb42dc3a9df28e065ed1d
Kevin <261039506+mccreeper1318@users.noreply.github.com>
2026-09-14 05:28:50 -0400

    Fix CI test runtime and update action versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.13.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 java {
